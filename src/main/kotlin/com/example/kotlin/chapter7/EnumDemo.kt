cd77c65897c9ddb5dd97e29e50ba8bec1c3a114b
riven_chris <hsj7107713@163.com>
2019-12-12 23:16:55 +0800

    commit

--- src/main/kotlin/com/example/kotlin/chapter7/EnumDemo.kt
+++ src/main/kotlin/com/example/kotlin/chapter7/EnumDemo.kt
@@ -1,5 +1,8 @@
 package com.example.kotlin.chapter7
 
+import java.util.function.BinaryOperator
+import java.util.function.IntBinaryOperator
+
 fun main(args: Array<String>) {
     println(Color.RED.rgb)
     println(Color.RED.name)
@@ -20,19 +23,38 @@ enum class Color(var rgb: Int) {
 enum class ActivityLifeState {
     onCreate {
         override fun signal() = onCreate
+        override fun str(): String = "onCreate"
     },
 
     onStart {
-        override fun signal() = onStop
+        override fun signal() = onStart
+        override fun str(): String = "onStart"
     },
 
     onStop {
-        override fun signal() = onStart
+        override fun signal() = onStop
+        override fun str(): String = "onStop"
     },
 
     onDestroy {
         override fun signal() = onDestroy
+
+        override fun str(): String = "onDestroy"
     };
 
     abstract fun signal(): ActivityLifeState
+
+    abstract fun str(): String
+}
+
+//枚举类可以实现接口
+enum class IntArithmetics : BinaryOperator<Int>, IntBinaryOperator {
+    PLUS {
+        override fun apply(t: Int, u: Int): Int = t + u
+    },
+    TIMES {
+        override fun apply(t: Int, u: Int): Int = t * u
+    };
+
+    override fun applyAsInt(t: Int, u: Int) = apply(t, u)
 }
--- src/main/kotlin/com/example/kotlin/chapter7/SealedDemo.kt
+++ src/main/kotlin/com/example/kotlin/chapter7/SealedDemo.kt
@@ -3,6 +3,9 @@ package com.example.kotlin.chapter7
 //在某种意义上，sealed类是枚举类的扩展：枚举类型的值集合也是受限的，
 // 但每个枚举常量只存在一个实例，而密封类的一个子类可以有可包含状态的多个实例
 
+//虽然密封类也可以有子类，但是所有子类都必须在与密封类自身相同的文件中声明。
+//扩展密封类子类的类（间接继承者）可以放在任何位置，而无需在同一个文件中
+
 sealed class Expression
 
 class Unit : Expression()
@@ -11,7 +14,7 @@ data class Sum(val e1: Expression, val e2: Expression) : Expression()
 data class Multiply(val e1: Expression, val e2: Expression) : Expression()
 object NaN : Expression()
 
-
+//使用密封类的好处
 fun eval(expr: Expression): Double = when (expr) {
     is Unit -> 1.0
     is Const -> expr.number
