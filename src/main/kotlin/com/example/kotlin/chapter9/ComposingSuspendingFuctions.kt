1621f2d0bec74743f736d83076687ee4d9d35b1c
hsjiang <hsj7107713@163.com>
2020-05-07 18:52:03 +0800

    commit

---
+++ src/main/kotlin/com/example/kotlin/chapter9/ComposingSuspendingFuctions.kt
@@ -0,0 +1,52 @@
+package com.example.kotlin.chapter9
+
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+fun main() {
+//    measureTime1()
+    asyncMeasureTime()
+//    lazyAsyncMeasureTime()
+}
+
+fun measureTime1() = runBlocking {
+    val timeMills = measureTimeMillis {
+        val one = doSomethingUsefulOne()
+        val two = doSomethingUsefulTwo()
+        println("The answer is ${one + two}")
+    }
+    println("Completed in $timeMills ms")
+}
+
+fun asyncMeasureTime() = runBlocking {
+    val timeMills = measureTimeMillis {
+        val one = async { doSomethingUsefulOne() }
+        val two = async { doSomethingUsefulTwo() }
+        println("The answer is ${one.await() + two.await()}")
+    }
+    println("Completed in $timeMills ms")
+}
+
+fun lazyAsyncMeasureTime() = runBlocking {
+    val timeMills = measureTimeMillis {
+        val one = async(start = CoroutineStart.LAZY) { doSomethingUsefulOne() }
+        val two = async(start = CoroutineStart.LAZY) { doSomethingUsefulTwo() }
+        one.start()
+        two.start()
+        println("The answer is ${one.await() + two.await()}")
+    }
+    println("Completed in $timeMills ms")
+}
+
+suspend fun doSomethingUsefulOne(): Int {
+    delay(1000L)
+    return 13
+}
+
+suspend fun doSomethingUsefulTwo(): Int {
+    delay(1000L)
+    return 29
+}
